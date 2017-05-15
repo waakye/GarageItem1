c38b7446627b2e0966161e24e9d563963af1ba46
waakye <dailyomnivore@gmail.com>
2017-05-15 09:07:19 +0800

    Add EditorActivity and supporting files

---
+++ app/src/main/java/com/waakye/garageitem/EditorActivity.java
@@ -0,0 +1,66 @@
+package com.waakye.garageitem;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+/**
+ * Created by lesterlie on 5/15/17.
+ */
+
+public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the item's name */
+    private EditText nameEditText;
+
+    /** EditText field to enter the item's price */
+    private EditText priceEditText;
+
+    /** EditText field to enter the item's quantity */
+    private EditText quantityEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        nameEditText = (EditText)findViewById(R.id.edit_item_name);
+        priceEditText = (EditText)findViewById(R.id.edit_item_price);
+        quantityEditText = (EditText)findViewById(R.id.edit_item_quantity);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()){
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
