e6498d2ecdea21741a9966e6d581280d600bdc9a
waakye <dailyomnivore@gmail.com>
2017-05-15 10:06:21 +0800

    Add image selector method that returns the uri for a selected image

--- app/src/main/java/com/waakye/garageitem/EditorActivity.java
+++ app/src/main/java/com/waakye/garageitem/EditorActivity.java
@@ -1,10 +1,17 @@
 package com.waakye.garageitem;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -12,6 +19,9 @@ import android.widget.EditText;
  */
 
 public class EditorActivity extends AppCompatActivity {
+
+    /** Tag for logging errors */
+    private static final String LOG_TAG = CatalogActivity.class.getSimpleName();
 
     /** EditText field to enter the item's name */
     private EditText nameEditText;
@@ -21,6 +31,18 @@ public class EditorActivity extends AppCompatActivity {
 
     /** EditText field to enter the item's quantity */
     private EditText quantityEditText;
+
+    /** Variables and constants related to image picker */
+    private static final int PICK_IMAGE_REQUEST = 0;
+//    private static final int SEND_MAIL_REQUEST = 1;
+
+    private static final String STATE_URI = "STATE_URI";
+
+//    private ImageView mImageView;
+//    private TextView mTextView;
+    private Button imageButton;
+
+    private Uri mUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +54,16 @@ public class EditorActivity extends AppCompatActivity {
         priceEditText = (EditText)findViewById(R.id.edit_item_price);
         quantityEditText = (EditText)findViewById(R.id.edit_item_quantity);
 
+//        mTextView = (TextView) findViewById(R.id.image_uri);
+//        mImageView = (ImageView) findViewById(R.id.image);
+
+        imageButton = (Button)findViewById(R.id.add_image_button);
+        imageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openImageSelector();
+            }
+        });
     }
 
     @Override
@@ -63,4 +95,41 @@ public class EditorActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void openImageSelector() {
+        Log.e(LOG_TAG, "openImageSelector() called ...");
+        Intent intent;
+
+        if (Build.VERSION.SDK_INT < 19) {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+        } else {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+        }
+
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    /**
+     * After the user selects a document in the picker, onActivityResult() gets called.
+     * The resultData parameter contains the URI that points to the selected document.
+     * Extract the URI using getData().  When you have it, you can use it to retrieve the document
+     * the user wants
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        Log.e(LOG_TAG, "onActivityResult() called ...");
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            // The resultData parameter contains the URI that points to the selected document.
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i(LOG_TAG, "Uri: " + uri.toString());  // The code works to here
+            }
+        }
+    }
 }
