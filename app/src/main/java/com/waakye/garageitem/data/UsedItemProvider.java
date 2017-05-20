a67c4273e5f897734cb05a816d6e1ed7da331729
waakye <dailyomnivore@gmail.com>
2017-05-20 10:53:40 +0800

    Implement ContentProvider query() method

--- app/src/main/java/com/waakye/garageitem/data/UsedItemProvider.java
+++ app/src/main/java/com/waakye/garageitem/data/UsedItemProvider.java
@@ -1,9 +1,11 @@
 package com.waakye.garageitem.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
@@ -61,7 +63,43 @@ public class UsedItemProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch(match) {
+            case USED_ITEMS:
+                // For the USED_ITEMS code, query the used_items table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor could
+                // contain multiple rows of the used_items table
+                cursor = database.query(UsedItemContract.UsedItemEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            case USED_ITEM_ID:
+                // For the USED_ITEM_ID code, extract out the ID from the URI.  For an example URI
+                // such as "content://com.waakye.garageitem.used_items/used_items/3", the
+                // selection will be "__id=?" and the selection argument will be a String array
+                // containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?".  Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = UsedItemContract.UsedItemEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+
+                // This will perform a query on the used_items table where the _id equals 3 to
+                // return a Cursor containing that row of the table.
+                cursor = database.query(UsedItemContract.UsedItemEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     @Override
