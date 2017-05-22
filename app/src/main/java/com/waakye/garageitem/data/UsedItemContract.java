797bc98d24ecadaade133bd121b719f37215310a
waakye <dailyomnivore@gmail.com>
2017-05-22 11:59:09 +0800

    Add constants to UsedItemContract for getType() method in UsedItemProvider class

--- app/src/main/java/com/waakye/garageitem/data/UsedItemContract.java
+++ app/src/main/java/com/waakye/garageitem/data/UsedItemContract.java
@@ -1,5 +1,6 @@
 package com.waakye.garageitem.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -39,7 +40,20 @@ public final class UsedItemContract {
     public static final class UsedItemEntry implements BaseColumns {
 
         /** The content URI to access the used_item data in the provider. */
-        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_USED_ITEMS);
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,
+                PATH_USED_ITEMS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of used_items
+         */
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
+                + "/" + CONTENT_AUTHORITY + "/" + PATH_USED_ITEMS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single used_item
+         */
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+                + "/" + PATH_USED_ITEMS;
 
         /** Name of database table for used_items */
         public final static String TABLE_NAME = "used_items";
